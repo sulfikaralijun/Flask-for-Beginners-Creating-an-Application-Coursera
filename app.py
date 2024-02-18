f1bc0d6e917821855369112a72a29e8f6c8ca038
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 00:15:11 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/hotels/')
+def display_hotels():
+  return render_template('template.html')
+
+if __name__ == '__main__':
+  app.run()
