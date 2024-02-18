2f97aa138044be3294965a44d897ea02f784b7fb
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 00:19:17 +0800

    update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+hotel_ratings = [
+    {'ID': 1,'name': 'MapleTree', 'rating': 4},
+    {'ID': 2,'name': 'Regents', 'rating': 5},
+    {'ID': 3,'name': 'DropInn', 'rating': 3},
+]
+
 @app.route('/hotels/')
 def display_hotels():
   return render_template('template.html')
